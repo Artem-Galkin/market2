00ed5cf00961807c59a4d7a70ba7b5890a365fba
Artem-Galkin <99556941+Artem-Galkin@users.noreply.github.com>
2022-03-20 10:42:55 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,43 @@
+<?php
+
+include "./avto/bmw.php";
+include "./avto/mercedes.php";
+
+// проверка
+if(isset($_GET['avto'])){
+
+    switch ($_GET['avto']) {
+        case "bmw":{
+            echo "ПОльзователь ищет запчасти по BMW";
+
+            $newClass = new Bmw("BMW");
+            $newNever = new Bmw();
+            // $newClass ->getName();
+            $newNever ->getName();
+        }
+
+        break;
+
+
+        case "mercedes":{
+            echo "Пользователь ищет запчасти по mercedes";
+
+
+        }
+
+        break;
+
+        case "mercedes":{
+            echo "Пользователь ищет запчасти по mercedes";
+
+
+        }
+
+        break;
+
+
+
+
+
+    }
+}
